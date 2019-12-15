b6a1aee0c122b9d907292c0b5a2bc3099c1cdd70
Kipras Melnikovas <kipras@kipras.org>
2019-12-15 21:48:17 +0200

    .eslintrc.js: Create a custom share-able config & use it! 🚀
    https://github.com/sarpik/eslint-config-sarpik
    
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,42 +2,17 @@
 // .eslintrc.js
 
 /**
-eslint ~/.eslintrc.js --ignore-pattern '!.eslintrc.js' --fix
+ * Kipras Melnikovas's <kipras@kipras.org> (https://kipras.org)
+ * ESLint config with Prettier and TypeScript
+ *
+ * https://github.com/sarpik/eslint-config-sarpik
+ * https://npmjs.com/package/eslint-config-sarpik
+ *
 */
 
-/**
- * @sarpik eslint config,
- * available at https://github.com/sarpik/voidrice/blob/master/.eslintrc.js
- *
- * Supports TypeScript!
- * (
- *		https://javascriptplayground.com/typescript-eslint/
- *	&	https://www.robertcooper.me/using-eslint-and-prettier-in-a-typescript-project
- * )
- *
- */
-
 module.exports = {
-	extends: ["wesbos", "plugin:@typescript-eslint/recommended"],
-	parser: "@typescript-eslint/parser" /** leggo typescript! */,
-	plugins: ["@typescript-eslint"],
-	rules: {
-		"no-console": "off",
-		quotes: [2, "double", { allowTemplateLiterals: true /** avoidEscape: dunno */ }],
-		"prettier/prettier": [
-			"error",
-			{
-				trailingComma: "es5",
-				printWidth: 120,
-				tabWidth: 4,
-				useTabs: true,
-				arrowParens: "always",
-				breakBeforeElse: true,
-				disableLanguages: [],
-				flattenTernaries: true,
-				proseWrap: "never",
-				htmlWhitespaceSensitivity: "ignore",
-			},
-		],
-	},
-};
+	extends: [
+		"eslint-config-sarpik"
+	]
+}
+
