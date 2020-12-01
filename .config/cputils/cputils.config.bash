a550228297b9287cf94bd848949eb80cfaa4bc20
Kipras Melnikovas <kipras@kipras.org>
2020-12-01 22:40:25 +0200

    cputils: create configs
    https://github.com/kiprasmel/cputils

---
+++ .config/cputils/cputils.config.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# cputils.config.bash
+
+open_with_editor() {
+	# exit 0 # delete this line and choose your editor if you want to
+
+	nvim "$1"
+	# emacs "$1"
+	# geany "$1" &
+	# code "$1"
+}
+export -f open_with_editor
+
